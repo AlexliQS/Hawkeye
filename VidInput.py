7b7390895465fc0e12339615ee2f91e5edef5d5d
AlexliQS <137822404+AlexliQS@users.noreply.github.com>
2023-08-17 12:05:40 -0700

    Update VidInput.py

--- VidInput.py
+++ VidInput.py
@@ -12,8 +12,8 @@ import tensorflow_hub as hub
 import sys
 import warnings
 import time
-import ffmpeg
-import glob
+
+
 
 
 #/home/orin_nx/Desktop/LTV/HighZoom1.ts
@@ -32,44 +32,39 @@ def streamRead():
         exit(-1)
     print('Video found')
     frames = 0
-    
+    '''
     shutil.rmtree("/home/orin_nx/Desktop/Frames")
     os.mkdir("/home/orin_nx/Desktop/Frames")
-    time1 = time.perf_counter()
+    '''
     i=0
-    print("Processing video...")
     while True:
         ret, frame = cap.read()
         if not ret:
             print('frame empty')
             break
-        resized_frame = resizeSquare(frame, 1280, 1280)
+        resized_frame = resizeSquare(frame, 800, 1333)
         #modelDet(resized_frame)
         #cv2.imshow('image', resized_frame)
-        odimg = modelDet(resized_frame)
+        modelDet(resized_frame)
         print(resized_frame.shape)
         i+=1
         if cv2.waitKey(34)&0XFF == ord('q'):
-            #print(processTimeList)
+            print(processTimeList)
             break
-        
+        '''
         #if frames >=900:
             #continue
         out_path = "/home/orin_nx/Desktop/Frames"
         frame_name = 'Frame' + str(i) + '.jpg'
-        cv2.imwrite(os.path.join(out_path, frame_name), odimg)
+        #cv2.imwrite(os.path.join(out_path, frame_name), resized_frame)
         frames+=1
-    time2 = time.perf_counter()
-    processTime = time2 - time1
-    print(processTime)
-    vidPlay()
-
-    #destroy window
+        '''
     cap.release()
     cv2.destroyAllWindows()
 
 def fileRead():
     global processTimeList
+    processTimeList = []
     pathtovid = input("Paste the path to the video you would like displayed: ")
     pathtovid = pathtovid.strip('"')
     print(pathtovid)
@@ -85,22 +80,29 @@ def fileRead():
     
     shutil.rmtree("/home/orin_nx/Desktop/Frames")
     os.mkdir("/home/orin_nx/Desktop/Frames")
-    time1 = time.perf_counter()
+    frameSkip = 0
     i=0
-    print("Processing video...")
     while True:
         ret, frame = fileCap.read()
         if not ret:
             print('frame empty')
             break
-        resized_frame = resizeSquare(frame, 1280, 1280)
+        resized_frame = resizeSquare(frame, 800, 1333)
         #modelDet(resized_frame)
         #cv2.imshow('image', resized_frame)
-        odimg = modelDet(resized_frame)
-        print(resized_frame.shape, frames)
+        if frames==0:
+            odimg = modelDet(resized_frame)
+        elif frameSkip!=2:
+            odimg = modelStat(resized_frame)
+            frameSkip+=1
+        else:
+            odimg = modelDet(resized_frame)
+            frameSkip = 0
+            print("DETECGTION")
+        print(resized_frame.shape)
         i+=1
         if cv2.waitKey(34)&0XFF == ord('q'):
-            #print(processTimeList)
+            print(processTimeList)
             break
         
         #if frames >=900:
@@ -109,54 +111,25 @@ def fileRead():
         frame_name = 'Frame' + str(i) + '.jpg'
         cv2.imwrite(os.path.join(out_path, frame_name), odimg)
         frames+=1
-    time2 = time.perf_counter()
-    processTime = time2 - time1
-    print(processTime)
-    vidPlay()
-    #fileCap.release()
-    #cv2.destroyAllWindows()
-
-
-def vidPlay():
-    #(ffmpeg.input('/home/orin_nx/Desktop/Frames/*.jpg', pattern_type='glob', framerate=25).output('movie.mp4').run())
-    image_folder = "/home/orin_nx/Desktop/Frames/"
-    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
-    images.sort(key=lambda x: int(x.replace("Frame", "").replace(".jpg", "")))
-    frame = cv2.imread(os.path.join(image_folder, images[0]))
-    height, width, layers = frame.shape
-
-    video = cv2.VideoWriter('video.avi', 0, 17, (width,height))
-
-    for image in images:
-        video.write(cv2.imread(os.path.join(image_folder, image)))
-
+        
+    fileCap.release()
     cv2.destroyAllWindows()
-    video.release()
-
 
 def resizeSquare(im, target_width, target_height):
     # Calculate the scaling factor based on the target dimensions and the larger dimension of the original image
     scale_factor_width = target_width / max(im.shape[1], im.shape[0])
     scale_factor_height = target_height / max(im.shape[0], im.shape[1])
     scale_factor = min(scale_factor_width, scale_factor_height)
-
-    # Calculate the new dimensions after resizing
     new_width = int(im.shape[1] * scale_factor)
     new_height = int(im.shape[0] * scale_factor)
-
-    # Resize the image using the calculated dimensions
     resized_im = cv2.resize(im, (new_width, new_height))
-
-    # Create a canvas of the target dimensions
     canvas = np.zeros((target_height, target_width, 3), dtype=np.uint8)
-
-    # Calculate the position to paste the resized image in the center of the canvas
     ax, ay = (target_width - new_width) // 2, (target_height - new_height) // 2
-
-    # Paste the resized image onto the canvas
     canvas[ay:ay + new_height, ax:ax + new_width] = resized_im
 
     return canvas
+
+
 
 
 def read_label_map(label_map_path):
@@ -185,9 +158,10 @@ def read_label_map(label_map_path):
   return items
 
 
+
 def modelDet(img):
     global processTimeList
-    
+    global global_boxes, global_classes, global_scores
     # read image and preprocess
     
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -197,12 +171,50 @@ def modelDet(img):
     h, w, _ = img.shape
 
     input_tensor = np.expand_dims(img, 0)
-    #time1=time.perf_counter()
     # predict from model
     resp = model(input_tensor)
-    #time2 = time.perf_counter()
+    #save current boxes for static box
+    global_boxes = resp['detection_boxes']
+    global_classes = resp['detection_classes']
+    global_scores = resp['detection_scores']
     # iterate over boxes, class_index and score list
     for boxes, classes, scores in zip(resp['detection_boxes'].numpy(), resp['detection_classes'], resp['detection_scores'].numpy()):
+        classes = np.vectorize(convert_to_string)(classes)
+        for box, cls, score in zip(boxes, classes, scores): # iterate over sub values in list
+            if score > 0.3: 
+                ymin = int(box[0] * h)
+                xmin = int(box[1] * w)
+                ymax = int(box[2] * h)
+                xmax = int(box[3] * w)
+                # write classname for bounding box
+                #cls = tf.cast(cls, tf.int32)
+                cls = cls.astype(float)
+                cls = cls.astype(int)
+                #print(type(cls))
+                #print(classes)
+                cv2.putText(img, classes[cls], (xmin, ymin-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
+                # draw on image
+                cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (128, 0, 128), 4)
+    # convert back to bgr and save image
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.imshow("image", img)
+    return img
+    #processTime = time2 - time1
+    #processTimeList.append(processTime)
+
+def modelStat(img):
+    global processTimeList
+    global global_boxes, global_classes, global_scores
+    # read image and preprocess
+    #print(global_boxes, '\n' + global_classes, '\n' + global_scores)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    print("Boxes\t\t\tClasses\t\tScores")
+    for box, classes, score in zip(global_boxes, global_classes, global_scores):
+        print(f"{box}\t{classes}\t{score}")
+    # get height and width of image
+    h, w, _ = img.shape
+
+    for boxes, classes, scores in zip(global_boxes.numpy(), global_classes, global_scores.numpy()):
         classes = np.vectorize(convert_to_string)(classes)
         for box, cls, score in zip(boxes, classes, scores): # iterate over sub values in list
             if score > 0.3: 
@@ -220,12 +232,11 @@ def modelDet(img):
                 cv2.putText(img, classes[cls], (xmin, ymin-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
                 # draw on image
                 cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (128, 0, 128), 4)
+                print("RECKTANGOW")
     # convert back to bgr and save image
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    #cv2.imshow("image", img)
+    cv2.imshow("image", img)
     return img
-    #processTime = time2 - time1
-    #processTimeList.append(processTime)
 
 def convert_to_string(x):
     return str(x)
@@ -234,8 +245,8 @@ def main():
     global model
     #model = hub.load("https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1")
     #model = hub.load("https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_800x1333/1")
-    model = hub.load("https://tfhub.dev/tensorflow/efficientdet/d0/1")
     #model = tf.saved_model.load("/home/orin_nx/Downloads/efficientdet_d1_coco17_tpu-32/saved_model")
+    model = hub.load("/home/orin_nx/Desktop/")
     print("MODEL LOAD")
     read_label_map("/home/orin_nx/Desktop/mscoco_label_map.pbtxt")
     while True: 
